eb03907456b769873574b14ea8d1f42190fd9af6
vkstheone <vks7pp2gmail.com>
2024-01-19 22:12:17 -0300

    first commit

---
+++ components/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header>Palmeiras</header>;
+};
+
+export default Header;
---
+++ components/Nav.js
@@ -0,0 +1,77 @@
+// icons
+import {
+  HiHome,
+  HiUser,
+  HiViewColumns,
+  HiRectangleGroup,
+  HiChatBubbleBottomCenterText,
+  HiEnvelope,
+} from 'react-icons/hi2';
+
+// nav data
+export const navData = [
+  { name: 'home', path: '/', icon: <HiHome /> },
+  { name: 'about', path: '/about', icon: <HiUser /> },
+  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
+  { name: 'work', path: '/work', icon: <HiViewColumns /> },
+  {
+    name: 'testimonials',
+    path: '/testimonials',
+    icon: <HiChatBubbleBottomCenterText />,
+  },
+  {
+    name: 'contact',
+    path: '/contact',
+    icon: <HiEnvelope />,
+  },
+];
+
+import Link from 'next/link';
+
+import { useRouter } from 'next/router';
+
+
+const Nav = () => {
+  const router = useRouter();
+  const pathname = router.pathname;
+
+  
+  
+  
+  
+  
+  
+  return (
+    <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto 
+    xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
+    {/* inner */}
+    
+    
+    
+    <div className='flex w-full xl:flex-col items-center justify-between 
+    xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py8 bg-wite/10
+    backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
+        {navData.map((link, index) =>{
+          return <Link href={link.path} key={index}>
+            
+            <div>
+              
+              {link.icon}
+            
+              </div>
+            
+            </Link>
+        })}
+    </div>
+
+
+
+
+
+
+''
+    </nav>
+  )  
+};
+
+export default Nav;
