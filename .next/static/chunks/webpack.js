e1d6b5cccae4a738aef21b77a8b93450e8607814
vkstheone <vks7pp@gmail.com>
2024-01-30 15:14:39 -0300

    sexto commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a0408a29fadb1503"; }
+/******/ 		__webpack_require__.h = function() { return "9db7ae42ea6da259"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/work/index.js
+++ pages/work/index.js
@@ -10,6 +10,52 @@ import { fadeIn } from '../../variants'
 const Work = () => {
   return (<div className='h-full bg-primary/30 py-36 flex itemns-center'> 
           <Circles />
+          <div className='container mx-auto'>
+            <div className='flex flex-xol xl:flex-row gap-x-8'>
+              <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
+                <motion.h2 
+                  variants={fadeIn('up', 0.2)}
+                  initial='hidden'
+                  animate='show'
+                  exit='hidden'
+                  className='h2 xl:mt-8'
+                >
+                  Meus serviços <span className='text-accent'>.</span>
+
+                </motion.h2>
+                <motion.p 
+                  variants={fadeIn('up', 0.4)}
+                  initial='hidden'
+                  animate='show'
+                  exit='hidden'
+                  className='mb-4 max-w-[400px] mx-auto lg:mx-0'
+                
+                
+                
+                > loresaljkckhaskldfjhasçlkdjhaslkdjaslkdasdasljdkhaskldasdasdasdasdasdasdas
+                 lore
+
+                </motion.p>
+              </div>
+              
+                <motion.div 
+                variants={fadeIn('down', 0.6)}
+                initial='hidden'
+                animate='show'
+                exit='hidden'
+                className='w-full xl:max-w-[65%]'
+                >
+                  <WorkSlider/>
+
+                </motion.div>
+              
+              </div> 
+
+
+          
+          
+          
+          </div>
       
       
       
