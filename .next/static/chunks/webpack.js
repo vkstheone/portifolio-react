1dfbc50b37cad65707ba5b4f0f1051e2de81fd7b
vkstheone <vks7pp@gmail.com>
2024-01-30 23:18:28 -0300

    configurando envio de email

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9ba6b09b4f298d5e"; }
+/******/ 		__webpack_require__.h = function() { return "25c0333ee742c71d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -4,8 +4,9 @@ import Circles from '/components/Circles'
 import {BsArrowRight} from 'react-icons/bs'
 import {motion} from 'framer-motion'
 import {fadeIn} from '../../variants'
+import { useState } from 'react'
+import emailjs from '@emailjs/browser'
 
-//import { useEffect, useState } from 'react'
 //import axios from '/Utils/axios'
 //import { data } from 'autoprefixer'
 //import { useDispatch } from 'react-redux'
@@ -13,52 +14,113 @@ import {fadeIn} from '../../variants'
 
  
 const Contact = () => {
-/*  const [name,setname] = useState("")
-  const [email,setemail] = useState("")
-  const [subject,setsubject] = useState("")
-  const [message,setmessage] = useState("")
-  const [User,setUser] = useState("")
+  
+  
+  const [name, setname] = useState("")
+  const [email, setemail] = useState("")
+  const [assunto, setassunto] = useState("")
+  const [message, setmessage] = useState("")
+  //const [User,setUser] = useState("")
 
 
-  const dispatch = useDispatch();
+  function sendEmail(e) {
+    e.preventDefault();
 
-
-  const submitHandler = (e)=>{
-    const user = {
-      name:name,
-      email:email,
-      subject:subject,
-      message:message
+    if(name == '' || email == '' || assunto == '' || message == ''){
+      alert("Prencha todos os Campos")
+      return;
     }
-    dispatch(asyncsignupstudent(user))
-    console.log(user)
+
+    const templateParams = {
+      from_name: name,
+      message: message,
+      email: email
+    }
+    emailjs.send("service_6mp5u1m", "template_rsdaec2", templateParams, "6cUlfheRqokYbjWCB")
+    .then((response) =>{
+      console.log("EMAIL ENVIADO", response.status, response.text)
+      setname("")
+      setemail("")
+      setassunto("")
+      setmessage("")
+    }, (err) => {
+      console.log("Erro:", err)
+    })
   }
-
-  submitHandler() */
-
-  // useEffect(()=>{
-  //   submitHandler();  
-  // },[submitHandler])
 
 
 
   return (<div className='h-full bg-primary/30 '>
     <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
       <div className='flex flex-col w-full max-w-[700px]'>
-        <motion.h2 variants={fadeIn('up',0.2)} initial="hidden" animate="show" exit="hidden" className='h2 text-center mb-12 '> Contact&apos;s <span className='text-accent '>connect.</span></motion.h2>
-        <motion.form action="/signup" variants={fadeIn('up',0.4)} initial="hidden" animate="show" exit="hidden" className='flex-1 flex flex-col gap-6 w-full mx-auto '>
-          <div className="flex gap-x-6 w-full">
-            <input type="text" placeholder='name' className='input'  />
-            <input type="email" placeholder='Email' className='input' />
-          </div>
-          <div className=''>
-            <input type="text" placeholder='Subject' className='input' />
-            {/* <input type="text" placeholder='Message' className='input h-[200px] mt-8'/> */}
-            <textarea  placeholder='message' className='textarea mt-8 xl:h-40 h-20' ></textarea>
-          </div>
-          <input type="Submit" placeholder="Let's talk" className='btn rounded-full border cursor-pointer border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group' />
-          <BsArrowRight className='mt-[-58px] ml-[130px]'/>
-        </motion.form >
+        <motion.h2 variants={fadeIn('up',0.2)} initial="hidden" animate="show" exit="hidden" className='h2 text-center mb-12 '> Entre em  <span className='text-accent '>Contato.</span></motion.h2>
+
+
+
+              <form 
+              onSubmit={sendEmail}
+              action="/signup" 
+              variants={fadeIn('up',0.4)} 
+              initial="hidden" 
+              animate="show" exit="hidden" 
+              className='flex-1 flex flex-col gap-6 w-full mx-auto '
+              >
+              <div className="flex gap-x-6 w-full">
+                  <input 
+                  type="text" 
+                  placeholder='Nome' 
+                  className='input' 
+                  onChange={(e) => setname(e.target.value)} 
+                  value={name}
+                  />
+                  
+                  
+                  <input type="email" 
+                  placeholder='Email' 
+                  className='input' 
+                  onChange={(e) => setemail(e.target.value)} 
+                  value={email}/>
+              
+              
+              
+              </div>
+                
+            <div className=''>
+                  <input type="text" 
+                  placeholder='Assunto' 
+                  className='input' 
+                  onChange={(e) => setassunto(e.target.value)} 
+                  value={assunto}/>
+                  {/* <input type="text" placeholder='Message' className='input h-[200px] mt-8'/> */}
+                  
+                  <textarea  
+                  placeholder='Digite Sua Mensagem ...' 
+                  className='textarea mt-8 xl:h-40 h-20'
+                  onChange={(e) => setmessage(e.target.value)} 
+                  value={message} >
+                  </textarea>
+            </div>
+                
+                
+                <input type="Submit"
+                placeholder="Let's talkssss" 
+                className='btn rounded-full border 
+                cursor-pointer border-white/50 
+                max-w-[170px] px-8 transition-all 
+                duration-300 flex items-center 
+                justify-center overflow-hidden 
+                hover:border-accent group' 
+                />
+                
+                <BsArrowRight className='mt-[-58px] ml-[130px]'/>
+              
+              
+              
+              </form >
+            
+
+
+
       </div>
     </div>
   </div>);
